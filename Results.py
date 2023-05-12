8904911ba174d76cf80b9dc17f47c506b836f2eb
ssafreed <133225257+ssafreed@users.noreply.github.com>
2023-05-12 19:38:31 +0530

    Update Results.py

--- Results.py
+++ Results.py
@@ -15,11 +15,11 @@ driver.maximize_window()
 
 outputFile = io.open('Results.csv','w',newline='')
 outputWriter = csv.writer(outputFile)
-outputWriter.writerow(['Id','Name','SGPA'])
+outputWriter.writerow( ['Id','Name','Branch','SGPA'])
 
 id_list = []
 pwd_list = []
-with open('data.txt','r') as f:
+with open('id.txt','r') as f:
     l = f.readlines()
     # print(l)
     for i in l:
@@ -28,40 +28,52 @@ with open('data.txt','r') as f:
 
 
 for i in range(len(id_list)):
-    usr = driver.find_element(By.NAME, "username")
-    usr.send_keys(id_list[i])
-    pwd = driver.find_element(By.NAME,"password")
-    pwd.send_keys(pwd_list[i])
+    try:
+        usr = driver.find_element(By.NAME, "username")
+        usr.send_keys(id_list[i])
+        pwd = driver.find_element(By.NAME,"password")
+        pwd.send_keys(pwd_list[i])
 
-    btn = driver.find_element(By.NAME,"login")
-    btn.click()
+        btn = driver.find_element(By.NAME,"login")
+        btn.click()
+        # time.sleep(1)
+
+        find_puc = driver.find_element(By.LINK_TEXT,"AY19-20 Results")
+        find_puc.click()
+        find_SEM = driver.find_element(By.LINK_TEXT,"SEM-I")
+        find_SEM.click()
+        semResults = driver.find_element(By.LINK_TEXT,"SEM-1Results")
+        semResults.click()
+        try:
+            getID = driver.find_element(By.ID,"pd")
+            l = getID.text.split('\n')
+            data = []
+            for i in l:
+                if i.startswith('ID'):
+                    i = i.lstrip('ID : ')
+                    data.append(i)
+                if i.startswith('Branch :'):
+                    i = i.lstrip('Branch : ')
+                    data.append(i)
+                if i.startswith('Name'):
+                    i = i.lstrip('Name :')
+                    data.append(i)
+            # print(data)
+            txt = driver.find_element(By.TAG_NAME,'tbody')
+            # print(txt.text)
+            data.append(txt.text[-4:])
+            outputWriter.writerow(data)
+            print(data)
+            logout = driver.find_element(By.LINK_TEXT,"Logout")
+            logout.click()
+        except:
+            data = [id_list[i],'Data extraction error']
+    except:
+        data = [id_list[i],'Login was not successful']
+        outputWriter.writerow(data)
     # time.sleep(1)
 
-    find_puc = driver.find_element(By.LINK_TEXT,"AY19-20 Results")
-    find_puc.click()
-    find_SEM = driver.find_element(By.LINK_TEXT,"SEM-I")
-    find_SEM.click()
-    semResults = driver.find_element(By.LINK_TEXT,"SEM-1Results")
-    semResults.click()
 
-    getID = driver.find_element(By.ID,"pd")
-    l = getID.text.split('\n')
-    data = []
-    for i in l:
-        if i.startswith('ID'):
-            i = i.lstrip('ID : ')
-            data.append(i)
-        if i.startswith('Name'):
-            i = i.lstrip('Name : ')
-            data.append(i)
-    # print(data)
-    txt = driver.find_element(By.TAG_NAME,'tbody')
-    # print(txt.text)
-    data.append(txt.text[-4:])
-    outputWriter.writerow(data)
-    print(data)
-    logout = driver.find_element(By.LINK_TEXT,"Logout")
-    logout.click()
-    # time.sleep(1)
+
 
 driver.close()
